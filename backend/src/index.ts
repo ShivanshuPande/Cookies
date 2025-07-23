ebbe329f848ab233e3981ceacc7413aa624d6942
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-07-23 20:11:19 +0530

    created the apis

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,33 +1,45 @@
 import express from "express" ;
-import cookieParser from "cookie-parser";
-import cors from "cors" ;
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken";
+import cookieParser  from "cookie-parser";
+import cors from 'cors';
 
-
-const app = express() ;
-const JWT_SECRET  = "jwt1234"
-app.use(cookieParser());
-app.use(cors({
-    credentials: true , 
-    origin : "http://localhost:5173"
-}))
+const app = express();
+const JWT_SECRET = "jwt1234";
 app.use(express.json());
 
+app.use(cookieParser());
 
-app.post("/signin", (req,res)=>{
-    const email  : req.body.email;
-    const password : req.body.password;
+app.use(cors({
+    credentials: true ,
+    origin : "http://localhost:5173"
 
+}));
+
+
+app.post("/signin" , (req  , res )=> {
+    const email = req.body.email;
+    const password = req.body.password;
+    
     const token = jwt.sign({
-        id :1
-    } , JWT_SECRET);
-    res.cookie("token"  , token);
-    res.send("logged in!!!")
-
-} );
-
-
-app.get("/" , (req ,res)=>{
-
+        id : 1
+    } , JWT_SECRET)
+    res.cookie("token" , token)
+    res.send("You are logged in ")
 })
 
+app.get("/" ,(req ,res)=>{
+    const token = req.cookies.token
+
+    const payload = jwt.verify("token" , JWT_SECRET) as JwtPayload;
+    const userId = payload.id;
+    res.send({
+        userID : userId
+    })
+});
+
+
+app.post("/logout" ,(req,res )=>{
+    res.clearCookie('token')
+
+    res.send("congratulation , you have been successfully logged outz")
+})
